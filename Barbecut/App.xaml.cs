e274d4ce8100156ac37d7d5a66e92c5bce95044d
Gustavo Haubrich <gustavohaubrich15@gmail.com>
2025-02-07 13:45:20 -0300

    Add files via upload

---
+++ Barbecut/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Barbecut.Domain;
+using Barbecut.Pages.Events;
+using Barbecut.Pages.Home;
+using Barbecut.Repositories;
+
+namespace Barbecut
+{
+	public partial class App : Application
+	{
+		public static Repository<Event> EventRepository { get; private set; } = new Repository<Event>();
+		public static Repository<Person> PersonRepository { get; private set; } = new Repository<Person>();
+		public static Repository<Item> ItemRepository { get; private set; } = new Repository<Item>();
+		public static Repository<ItemPersonBought> ItemPersonBoughtRepository { get; private set; } = new Repository<ItemPersonBought>();
+
+		public App()
+		{
+			InitializeComponent();
+			var navigationPage = new NavigationPage();
+			MainPage = new NavigationPage(new HomePage());
+		}
+	}
+}
---
+++ Barbecut/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Maui.Controls.Hosting;
+using Barbecut.Pages.Events;
+using Barbecut.Pages.Event.Interface;
+using CommunityToolkit.Maui;
+
+namespace Barbecut
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.UseMauiCommunityToolkit()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("Inter-Regular.ttf", "InterRegular");
+				});
+
+#if DEBUG
+			builder.Logging.AddDebug();
+#endif
+			
+			return builder.Build();
+		}
+	}
+}
